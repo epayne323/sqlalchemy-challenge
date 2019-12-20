1dcd4472d4a0b0069ddb190afd02dff9903fea35
Eli Payne <epayne323@gmail.com>
2019-12-19 18:17:40 -0600

    fixed flask app (needed @ before home route). updated readme.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # sqlalchemy-challenge
+
+Exploratory Climate Analysis of Weather Data in Hawaii, using Flask to display analysis results on a webpage.
--- app.py
+++ app.py
@@ -20,19 +20,20 @@ Station = Base.classes.station
 
 app = Flask(__name__)
 
-app.route("/")
+@app.route("/")
 def welcome():
     """List all available api routes."""
     return \
     (
         f"Available Routes:<br/>"
+        f"<br/>"
         f"/api/v1.0/precipitation<br/>"
         f"/api/v1.0/stations<br/>"
         f"/api/v1.0/tobs<br/>"
-        f"/api/v1.0/<start><br/>"
-        f"/api/v1.0/<start>/<end><br/>"
+        f"/api/v1.0/start<br/>"
+        f"/api/v1.0/start/end<br/>"
         f"<br/>"
-        f"NOTE: please enter <start> and <end> dates as YYYY-MM-DD"
+        f"NOTE: please format dates <em>start</em> and <em>end</em> as YYYY-MM-DD"
     )
 
 
